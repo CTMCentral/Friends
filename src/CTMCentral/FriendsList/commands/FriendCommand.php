f3b22b3d0950afe0007190a92bf3c9acba773c91
provsalt <gooraymond39@gmail.com>
2020-07-20 19:51:05 +0800

    Undo use of Form virion instead depneding on HubTools forms

--- src/CTMCentral/FriendsList/commands/FriendCommand.php
+++ src/CTMCentral/FriendsList/commands/FriendCommand.php
@@ -4,12 +4,18 @@ namespace CTMCentral\FriendsList\commands;
 
 use CortexPE\Commando\BaseCommand;
 use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
 use pocketmine\plugin\PluginOwned;
+use pocketmine\utils\TextFormat as TF;
 
 class FriendCommand extends BaseCommand implements PluginOwned {
 
 	public function onRun(CommandSender $sender, string $aliasUsed, array $args): void{
-		// TODO: Implement onRun() method.
+		if (!$sender instanceof Player) {
+			$sender->sendMessage(TF::RED . "You can only run this command in game");
+			return;
+		}
+
 	}
 
 	protected function prepare(): void{
