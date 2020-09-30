3c38386a281631a792f5fce02c18381bd256a416
provsalt <gooraymond39@gmail.com>
2020-09-30 19:58:11 +0800

    I hope I saved it correctly

--- src/CTMCentral/Friends/commands/subcommands/admin.php
+++ src/CTMCentral/Friends/commands/subcommands/admin.php
@@ -1,1 +1,22 @@
 <?php
+
+namespace CTMCentral\Friends\commands\subcommands
+
+use CortexPE\Commando\args\RawStringArgument;
+use CortexPE\Commando\BaseSubCommand;
+use pocketmine\command\CommandSender;
+
+class admin extends BaseSubCommand {
+
+	protected function prepare(): void{
+		$this->registerArgument(0, new RawStringArgument("admin", true));
+	}
+
+	public function onRun(CommandSender $sender, string $aliasUsed, array $args): void{
+		if (!$sender->hasPermission("friends.admin")) return;
+
+		if ($args[0] === "addfriend") {
+			//TODO
+		}
+	}
+}
